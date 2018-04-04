d1a04e7d531696b50ef2eec497edff566e60eeef
Marc Guo <marqueguo@gmail.com>
2018-04-04 18:12:15 -0400

    Controller test class

---
+++ test/cs3500/animator/ControllerTests.java
@@ -0,0 +1,21 @@
+package cs3500.animator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import cs3500.animator.model.SimpleAnimation;
+import cs3500.animator.view.AbstractView;
+import cs3500.animator.view.TextView;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ControllerTests {
+  void testAction() {
+
+  }
+}
