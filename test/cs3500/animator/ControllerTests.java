f6db94c62e0e7785ec44728f3aa9b2a97b865b5a
Marc Guo <marqueguo@gmail.com>
2018-04-04 20:27:06 -0400

    Tests for Controller.

--- test/cs3500/animator/ControllerTests.java
+++ test/cs3500/animator/ControllerTests.java
@@ -1,21 +1,69 @@
 package cs3500.animator;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.awt.event.ActionEvent;
 
+import cs3500.animator.controller.Controller;
+import cs3500.animator.controller.IController;
 import cs3500.animator.model.SimpleAnimation;
-import cs3500.animator.view.AbstractView;
-import cs3500.animator.view.TextView;
+import cs3500.animator.model.SimpleAnimationModel;
+import cs3500.animator.view.InteractiveView;
+import cs3500.animator.view.ViewInterface;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
 
 public class ControllerTests {
-  void testAction() {
 
+  /**
+   * Tests the button links.
+   */
+  @Test
+  public void testAction() {
+    SimpleAnimationModel model = new SimpleAnimation();
+    InteractiveView view = new InteractiveView(model, 20);
+    IController controller = new Controller(model, view);
+    ActionEvent start = new ActionEvent(this, 0, "start");
+    ActionEvent restart = new ActionEvent(this, 0, "restart");
+    ActionEvent increaseTempo = new ActionEvent(this, 0, "increase tempo");
+    ActionEvent decreaseTempo = new ActionEvent(this, 0, "decrease tempo");
+    ActionEvent loop = new ActionEvent(this, 0, "loop");
+    ActionEvent addShape = new ActionEvent(this, 0, "add shape to subset");
+    ActionEvent viewSubset = new ActionEvent(this, 0, "view subset");
+    ActionEvent playSubset = new ActionEvent(this, 0, "play subset");
+    ActionEvent svgSubset = new ActionEvent(this, 0, "SVG subset");
+
+    try {
+      controller.action(start);
+      controller.action(restart);
+      controller.action(increaseTempo);
+      controller.action(decreaseTempo);
+      controller.action(loop);
+      controller.action(addShape);
+      controller.action(viewSubset);
+      controller.action(playSubset);
+      controller.action(svgSubset);
+    } catch (IllegalArgumentException e) {
+      assertFalse("false", false);
+    } catch (Throwable t) {
+      //do nothing since other exceptions are OK
+    }
+    assertTrue("OK", true);
+  }
+
+  /**
+   * Tests the exception for invalid button info.
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testActionException() {
+    SimpleAnimationModel model = new SimpleAnimation();
+
+    ViewInterface view = new InteractiveView(model, 20);
+    IController controller = new Controller(model, view);
+    ActionEvent start = new ActionEvent(this, 0, "haha");
+    controller.action(start);
   }
 }
+
